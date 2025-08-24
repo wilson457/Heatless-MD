d71108fe45b6ca54b5ed5d41b328c85b0a122aa9
Kelvin12350 <dianwilson66@gmail.com>
2025-08-24 03:38:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "E3_HACKER_MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiU0JUakZzbzdrdnhrTDloVWZ2UEQzek9Pdlk0NkVlVTF4dHNkT2NNNnpGOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZCsxZ2g0M25nOHFPVXFMdkxvRzF1WU5JbUhFdG9aMVlGWXMwMStKYzdnND0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJRS2F3Sm5XUGorY01oNGc5cFBaOVpDeG9kQ0JYMXpNRGYrd2V1ZFlVMEZRPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJKVVRYamZRNkJiT0tTNlJOemFkMStQbmNuRmNHMHoybytvNktyMmdxa2ljPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImFOQmdkWlRXREQxNk1Ya25yQmFwWFlzMnRmVG00NWNUSW8vb3hpYnZIMWc9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InNtS0xPWmV5LzdRMzhsb0E1bzhERTlUZFArZjZlOW1yK3dMK3h5UHRoVTQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSUdWNk0zdHo5OHlxZm9KQW8xQTNDTXhueDc5VmxXZXBUT3ZTRE10WFJHMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMVphak5oVUxsOGo1WGhXTW44SnB0eEhqYWFyVFlDSGd1SFpKMW5BcUJRRT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik5hNlZnR0FCdzN3V1kvRDV6OTFLR0tzUzg1Z2pTSGExb2Y5NXV4WUVQM3JXVmkxSTN6UkVpUWQ2TWFVWE1lalBZQkRUalZDaVMwQlg1OE1odWZYVGpnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6OTYsImFkdlNlY3JldEtleSI6IjdxdDZnT0Jzb253TzdyT0ZFT0R5ajlhKzhUUUZkcE1tMy9KZThtS3d3NzQ9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJyZWdpc3RlcmVkIjp0cnVlLCJwYWlyaW5nQ29kZSI6IlpHM1RRQkhIIiwibWUiOnsiaWQiOiI5MjMzMjg3NTY0NDU6MTVAcy53aGF0c2FwcC5uZXQiLCJsaWQiOiIxMDgzNzk3MDgwNjM3NzU6MTVAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNKSFB6M0lRM0t5Z3hRWVlBaUFBS0FBPSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJpQ1R0cHVMSGJFZCt3Z1N5cWRNaTNrdmhjK2oyL1JXbmhJcFZRamtSd3lZPSIsImFjY291bnRTaWduYXR1cmUiOiJ6bmt5ZTdtcmxVNWllOTY2SVlDazlTMXkybUQyMkc3ZkM3MXVZckhid2g1VktBU1BOb01rcEtZUVp5Z2VBc0ZzS0F6N29hTmNlaEdCQkhGOHE5bllBQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoicVcrTmFLVGlzTkUvZmNKNG5rbjd1L1RMVFh0L3dFbHZHY3UydXNzOTE1SlZqbS9oSDlvM1lGcU5JYnhiUUtvNTl4QXM1SGMxRXVyOGpRRTNKUW14aXc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MjMzMjg3NTY0NDU6MTVAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWWdrN2FiaXgyeEhmc0lFc3FuVEl0NUw0WFBvOXYwVnA0U0tWVUk1RWNNbSJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0EwSUJRPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzU1ODQ2MjQwLCJsYXN0UHJvcEhhc2giOiIzUjlaMzkiLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUpQRiJ9",
+SESSION_ID: process.env.SESSION_ID || "Heatless-MD~eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiU0JUakZzbzdrdnhrTDloVWZ2UEQzek9Pdlk0NkVlVTF4dHNkT2NNNnpGOD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZCsxZ2g0M25nOHFPVXFMdkxvRzF1WU5JbUhFdG9aMVlGWXMwMStKYzdnND0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJRS2F3Sm5XUGorY01oNGc5cFBaOVpDeG9kQ0JYMXpNRGYrd2V1ZFlVMEZRPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJKVVRYamZRNkJiT0tTNlJOemFkMStQbmNuRmNHMHoybytvNktyMmdxa2ljPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImFOQmdkWlRXREQxNk1Ya25yQmFwWFlzMnRmVG00NWNUSW8vb3hpYnZIMWc9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6InNtS0xPWmV5LzdRMzhsb0E1bzhERTlUZFArZjZlOW1yK3dMK3h5UHRoVTQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiSUdWNk0zdHo5OHlxZm9KQW8xQTNDTXhueDc5VmxXZXBUT3ZTRE10WFJHMD0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMVphak5oVUxsOGo1WGhXTW44SnB0eEhqYWFyVFlDSGd1SFpKMW5BcUJRRT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik5hNlZnR0FCdzN3V1kvRDV6OTFLR0tzUzg1Z2pTSGExb2Y5NXV4WUVQM3JXVmkxSTN6UkVpUWQ2TWFVWE1lalBZQkRUalZDaVMwQlg1OE1odWZYVGpnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6OTYsImFkdlNlY3JldEtleSI6IjdxdDZnT0Jzb253TzdyT0ZFT0R5ajlhKzhUUUZkcE1tMy9KZThtS3d3NzQ9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJyZWdpc3RlcmVkIjp0cnVlLCJwYWlyaW5nQ29kZSI6IlpHM1RRQkhIIiwibWUiOnsiaWQiOiI5MjMzMjg3NTY0NDU6MTVAcy53aGF0c2FwcC5uZXQiLCJsaWQiOiIxMDgzNzk3MDgwNjM3NzU6MTVAbGlkIn0sImFjY291bnQiOnsiZGV0YWlscyI6IkNKSFB6M0lRM0t5Z3hRWVlBaUFBS0FBPSIsImFjY291bnRTaWduYXR1cmVLZXkiOiJpQ1R0cHVMSGJFZCt3Z1N5cWRNaTNrdmhjK2oyL1JXbmhJcFZRamtSd3lZPSIsImFjY291bnRTaWduYXR1cmUiOiJ6bmt5ZTdtcmxVNWllOTY2SVlDazlTMXkybUQyMkc3ZkM3MXVZckhid2g1VktBU1BOb01rcEtZUVp5Z2VBc0ZzS0F6N29hTmNlaEdCQkhGOHE5bllBQT09IiwiZGV2aWNlU2lnbmF0dXJlIjoicVcrTmFLVGlzTkUvZmNKNG5rbjd1L1RMVFh0L3dFbHZHY3UydXNzOTE1SlZqbS9oSDlvM1lGcU5JYnhiUUtvNTl4QXM1SGMxRXVyOGpRRTNKUW14aXc9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MjMzMjg3NTY0NDU6MTVAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWWdrN2FiaXgyeEhmc0lFc3FuVEl0NUw0WFBvOXYwVnA0U0tWVUk1RWNNbSJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsInJvdXRpbmdJbmZvIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQ0EwSUJRPT0ifSwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzU1ODQ2MjQwLCJsYXN0UHJvcEhhc2giOiIzUjlaMzkiLCJteUFwcFN0YXRlS2V5SWQiOiJBQUFBQUpQRiJ9",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -13,7 +13,7 @@ AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 // make true if you want auto reply on status 
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY 𝙀3_𝙃𝘼𝘾𝙆𝙀𝙍_𝙈𝘿 ♥️☺️*",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY Heatless-MD ♥️☺️*",
 // set the auto reply massage on status reply  
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
 // set true false for anti delete     
@@ -27,13 +27,13 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/p4yxcn.png",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://cdn.kordai.biz.id/serve/HlEHZd6pgErX.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "E3_HACKER_MD",
+BOT_NAME: process.env.BOT_NAME || "Heatless-MD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "𝙀3_𝙃𝘼𝘾𝙆𝙀𝙍_𝙈𝘿",
+STICKER_NAME: process.env.STICKER_NAME || "Heatless-MD",
 // type sticker pack name 
 CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
@@ -41,13 +41,13 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923495178663",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "2349039409985",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "*E3_HACKER_MD Official*",
+OWNER_NAME: process.env.OWNER_NAME || "*Heatless-MD Official*",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ E3_HACKER_MD Official ❣️*",
+DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Heatless-MD Official ❣️*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/p4yxcn.png",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://cdn.kordai.biz.id/serve/HlEHZd6pgErX.jpg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> Apni Had May Raho Batmeez Insan 🫣",
 // add alive msg here 
